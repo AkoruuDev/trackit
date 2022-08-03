fbdcb0ad335612478eb2d1dea20851bd897cd36b
Akoruu.Dev <akoruu.dev@gmail.com>
2022-08-03 20:08:27 -0300

    BAS [Routes]: Add main pages 'Header.js', 'Main.js' and 'Footer.js'; Add pages from Main.js 'Login.js', 'Registration.js', 'Habits.js', 'Today.js' and 'Historic.js'; Add routes on App.js for pages from App

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+
+import Login from "./Pages/Log/Login";
+import Registration from "./Pages/Log/Registration";
+import Habits from "./Pages/Habits";
+import Today from "./Pages/Today";
+import Historic from "./Pages/Historic";
+
 export default function App() {
     return(
-        <></>
+        <BrowserRouter>
+            <Header />
+            <Main>
+                <Routes>
+                    <Route path="/" element={ <Login /> } />
+                    <Route path="/cadastro" element={ <Registration /> } />
+                    <Route path="/habitos" element={ <Habits /> } />
+                    <Route path="/hoje" element={ <Today /> } />
+                    <Route path="/historico" element={ <Historic /> } />
+                </Routes>
+            </Main>
+            <Footer />
+        </BrowserRouter>
     )
 }
