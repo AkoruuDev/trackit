4875bfe3361338c0bb5e7a397a76e06bc73d0740
Akoruu.Dev <akoruu.dev@gmail.com>
2022-08-03 20:32:48 -0300

    STYLE [Header]: Create style components for header using styled-components from App

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,41 @@
+import styled from "styled-components"
+
 export default function Header() {
     return (
-        <header>
-            <h1>TrackIt</h1>
-            <img src="" alt="profile" />
-        </header>
+        <MainHeader>
+            <Content>
+                <Title>TrackIt</Title>
+                <Profile src="" alt="profile" />
+            </Content>
+        </MainHeader>
     )
 }
+
+const MainHeader = styled.header`
+    width: 100vw;
+    heigth: 0px;
+    padding: 10px;
+    background-color: blue;
+    display: flex;
+    justify-content: center;
+`;
+
+const Content = styled.div`
+    width: 80vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const Title = styled.h1`
+    font-family: 'Playball', cursive;
+    color: #FFFFFF;
+    font-size: 38px;
+`;
+
+const Profile = styled.img`
+    width: 51px;
+    height: 51px;
+    background-color: #FFFFFF;
+    border-radius: 50%;
+`;
