a092431703a6b3e137513303a435ec56375423f7
Akoruu.Dev <akoruu.dev@gmail.com>
2022-08-04 18:28:40 -0300

    BAS [Registration]: Create structure for Registration.js file from App

--- src/components/Pages/Log/Registration.js
+++ src/components/Pages/Log/Registration.js
@@ -1,5 +1,20 @@
+import logo from "../../images/logo.png";
+
 export default function Registration() {
     return (
-        <></>
+        <div>
+            <div>
+                <img src={logo} alt="logotipo" />
+                <h1>TrackIt</h1>
+                <form>
+                    <input type="email" placeholder="e-mail" />
+                    <input type="password" placeholder="senha" />
+                    <input type="name" placeholder="nome" />
+                    <input type="picture" placeholder="foto" />
+                    <button type="submit">Cadastrar</button>
+                </form>
+                <p>Já tem uma conta? Faça login!</p>
+            </div>
+        </div>
     )
 }
