f2854ad9b4dfaabda5f6f878f045cccee486a42a
Akoruu.Dev <akoruu.dev@gmail.com>
2022-08-05 17:09:19 -0300

    FEAT [Axios]: Create promises for axios library from App

---
+++ src/components/services/trackit.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+const BASE_URL = 'https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit';
+
+
+function signUp() {
+    const promise = axios.get(`${BASE_URL}/auth/sign-up`);
+    return promise;
+}
+
+function doLogin() {
+    const promise = axios.get(`${BASE_URL}/auth/login`);
+    return promise;    
+}
+
+function createHabits() {
+    const promise = axios.get(`${BASE_URL}/habits`);
+    return promise;
+}
+
+function generateHabits() {
+    const promise = axios.get(`${BASE_URL}/habits`);
+    return promise;
+}
+
+function deleteHabits(HABIT_ID) {
+    const promise = axios.get(`${BASE_URL}/habits/${HABIT_ID}`);
+    return promise;
+}
+
+function getHabits() {
+    const promise = axios.get(`${BASE_URL}/habits/today`);
+    return promise;
+}
+
+function doneHabit(HABIT_ID) {
+    const promise = axios.get(`${BASE_URL}/habits/${HABIT_ID}/check`);
+    return promise;
+}
+
+function undoneHabit(HABIT_ID) {
+    const promise = axios.get(`${BASE_URL}/habits/${HABIT_ID}/uncheck`);
+    return promise;
+}
+
+function historic() {
+    const promise = axios.get(`${BASE_URL}/history/daily`);
+    return promise;    
+}
+
+export { signUp, doLogin, createHabits, generateHabits, deleteHabits, getHabits, doneHabit, undoneHabit, historic };
